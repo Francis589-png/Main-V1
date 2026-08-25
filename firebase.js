b1b5ff5f43f23b374814342d9c2633b85726ada5
Francis589-png <jusufrancis08@gmail.com>
2026-08-25 11:19:38 +0100

    feat: expose Firestore message deletion primitive

--- firebase.js
+++ firebase.js
@@ -1,42 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js';
-import {
-  getAuth,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  sendEmailVerification,
-  sendPasswordResetEmail,
-  signOut,
-  updateProfile,
-  setPersistence,
-  browserLocalPersistence
-} from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
-import {
-  getDatabase,
-  ref,
-  set,
-  update,
-  push,
-  get,
-  onValue,
-  serverTimestamp
-} from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js';
-import {
-  getFirestore,
-  collection,
-  doc,
-  setDoc,
-  getDoc,
-  getDocs,
-  updateDoc,
-  addDoc,
-  query,
-  where,
-  orderBy,
-  limit,
-  onSnapshot,
-  serverTimestamp as firestoreServerTimestamp
-} from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js';
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signOut, updateProfile, setPersistence, browserLocalPersistence } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
+import { getDatabase, ref, set, update, push, get, onValue, serverTimestamp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js';
+import { getFirestore, collection, doc, setDoc, getDoc, getDocs, updateDoc, deleteDoc, addDoc, query, where, orderBy, limit, onSnapshot, serverTimestamp as firestoreServerTimestamp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js';
 import { firebaseConfig } from './firebase-config.js';
 
 const configured = Object.values(firebaseConfig).every(value => value && !String(value).startsWith('YOUR_'));
@@ -54,38 +19,9 @@ if (configured) {
 }
 
 export {
-  configured,
-  app,
-  auth,
-  db,
-  firestore,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  sendEmailVerification,
-  sendPasswordResetEmail,
-  signOut,
-  updateProfile,
-  setPersistence,
-  browserLocalPersistence,
-  ref,
-  set,
-  update,
-  push,
-  get,
-  onValue,
-  serverTimestamp,
-  collection,
-  doc,
-  setDoc,
-  getDoc,
-  getDocs,
-  updateDoc,
-  addDoc,
-  query,
-  where,
-  orderBy,
-  limit,
-  onSnapshot,
-  firestoreServerTimestamp
+  configured, app, auth, db, firestore, onAuthStateChanged, signInWithEmailAndPassword,
+  createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signOut,
+  updateProfile, setPersistence, browserLocalPersistence, ref, set, update, push, get, onValue,
+  serverTimestamp, collection, doc, setDoc, getDoc, getDocs, updateDoc, deleteDoc, addDoc,
+  query, where, orderBy, limit, onSnapshot, firestoreServerTimestamp
 };

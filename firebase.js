04ee510cbea73bdc0ffb7f36b8655630625d8cae
Francis589-png <jusufrancis08@gmail.com>
2026-08-25 15:30:09 +0100

    fix: expose Firestore collection group queries

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js';
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signOut, updateProfile, setPersistence, browserLocalPersistence } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
 import { getDatabase, ref, set, update, push, get, onValue, serverTimestamp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js';
-import { getFirestore, collection, doc, setDoc, getDoc, getDocs, updateDoc, deleteDoc, addDoc, query, where, orderBy, limit, startAt, endAt, onSnapshot, serverTimestamp as firestoreServerTimestamp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js';
+import { getFirestore, collection, collectionGroup, doc, setDoc, getDoc, getDocs, updateDoc, deleteDoc, addDoc, query, where, orderBy, limit, startAt, endAt, onSnapshot, serverTimestamp as firestoreServerTimestamp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js';
 import { firebaseConfig } from './firebase-config.js';
 
 const requiredConfigKeys = ['apiKey', 'authDomain', 'projectId', 'storageBucket', 'messagingSenderId', 'appId'];
@@ -17,4 +17,4 @@ if (configured) {
   firestore = getFirestore(app);
   setPersistence(auth, browserLocalPersistence).catch(() => {});
 }
-export { configured, app, auth, db, firestore, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signOut, updateProfile, setPersistence, browserLocalPersistence, ref, set, update, push, get, onValue, serverTimestamp, collection, doc, setDoc, getDoc, getDocs, updateDoc, deleteDoc, addDoc, query, where, orderBy, limit, startAt, endAt, onSnapshot, firestoreServerTimestamp };
+export { configured, app, auth, db, firestore, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signOut, updateProfile, setPersistence, browserLocalPersistence, ref, set, update, push, get, onValue, serverTimestamp, collection, collectionGroup, doc, setDoc, getDoc, getDocs, updateDoc, deleteDoc, addDoc, query, where, orderBy, limit, startAt, endAt, onSnapshot, firestoreServerTimestamp };
